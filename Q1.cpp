47461729ec7abd1e4d796883532462d4b84c0fe4
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-08-17 06:46:12 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -11,10 +11,10 @@ double doubleVar = 19.122000;
 bool boolVar = false;
 
 //Displaying the values and sizes of all basic data types
-cout <<"The integer value is " <<intVar<< " and the size of the integer value is " <<sizeof(int)<< "."<< endl;
-cout <<"The float value is " <<floatVar<< " and the size of the float value is " <<sizeof(float)<< "."<< endl;
-cout <<"The character value is " <<charVar<< " and the size of the character value is " <<sizeof(char)<< "."<< endl;
-cout <<"The double value is " <<doubleVar<< " and the size of the double value is " <<sizeof(double)<< "."<< endl;
-cout <<"The boolean value is " <<boolVar<< " and the size of the boolean value is " <<sizeof(bool)<< "."<< endl;
+cout <<"The integer value is " <<intVar<< " and its size is " <<sizeof(int)<< "."<< endl;
+cout <<"The float value is " <<floatVar<< " and its size is " <<sizeof(float)<< "."<< endl;
+cout <<"The character value is " <<charVar<< " and its size is " <<sizeof(char)<< "."<< endl;
+cout <<"The double value is " <<doubleVar<< " and its size is " <<sizeof(double)<< "."<< endl;
+cout <<"The Boolean value is " <<boolVar<< " and its size is " <<sizeof(bool)<< "."<< endl;
 return 0;
 }
