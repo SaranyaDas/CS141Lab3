38047a9768e918256cddd3ac14dbed4741663d68
SaranyaDas <42189706+SaranyaDas@users.noreply.github.com>
2018-08-17 04:39:02 +0000

    Update Q2float.cpp

--- Q2float.cpp
+++ Q2float.cpp
@@ -1,7 +1,6 @@
 //Library
 #include <iostream>
 using namespace std;
-
 int main() {
 //Declaring variables in desired data type and asking for input
 float n1, n2;
@@ -9,12 +8,12 @@ cout <<"Enter a number:";
 cin >> n1;
 cout <<"Enter another number:";
 cin >> n2;
-//Performing the arithmetic functions
+//Performing arithmetic functions
 float sum = n1 + n2;
 float diff = n1 - n2;
 float product = n1*n2;
 float div = n1/n2;
-//Displaying the output of the results
+//Displaying output of results
 cout <<"The sum of the numbers is " <<sum<< "."<< endl;
 cout <<"The difference of the numbers is " <<diff<< "."<< endl;
 cout <<"The product of the numbers is " <<product<< "."<< endl;
